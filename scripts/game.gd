b6a20932371fb34e9bbdaec5821bc58b3a0b43a8
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-08-17 23:35:35 +0100

    better rotation

--- scripts/game.gd
+++ scripts/game.gd
@@ -18,7 +18,7 @@ const MAX_ROTATION_SPEED = 90
 
 @export var decay_speed = 0.2
 
-@export var rotation_speed := 0:
+@export var rotation_speed := 0.0:
 	set(value):
 		if value > MAX_ROTATION_SPEED:
 			rotation_speed = MAX_ROTATION_SPEED
@@ -30,6 +30,13 @@ const MAX_ROTATION_SPEED = 90
 func _process(delta):
 	seesaw_length -= decay_speed * delta
 	seesaw.rotation_degrees += rotation_speed * delta
+	
+	if player.is_on_floor():
+		var player_position : float = (player.position.x - 3000) / 48
+		var player_position_fraction := player_position / seesaw_length
+		#rotation_speed += (MAX_ROTATION_SPEED * player_position_fraction) * delta
+		rotation_speed = lerp(rotation_speed, (MAX_ROTATION_SPEED**2) * player_position_fraction * delta, 1)
+		
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
