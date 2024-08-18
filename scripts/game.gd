e7b1c1423c673239a280064b74db86ed95928ffa
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-08-18 21:10:48 +0100

    ground pound on seesaw

--- scripts/game.gd
+++ scripts/game.gd
@@ -19,14 +19,7 @@ const MAX_ROTATION_SPEED = 75
 
 @export var decay_speed = 0.1
 
-@export var rotation_speed := 0.0:
-	set(value):
-		if value > MAX_ROTATION_SPEED:
-			rotation_speed = MAX_ROTATION_SPEED
-		elif value < -MAX_ROTATION_SPEED:
-			rotation_speed = -MAX_ROTATION_SPEED
-		else:
-			rotation_speed = value
+@export var rotation_speed := 0.0
 			
 @export var score := 0:
 	set(value):
@@ -35,27 +28,32 @@ const MAX_ROTATION_SPEED = 75
 		
 var score_delta_tracker := 0.0
 
+var ground_pound_timer := 0.0
+
 func _process(delta):
 	seesaw_length -= decay_speed * delta
 	seesaw.rotation_degrees += rotation_speed * delta
 	
-	if player.is_on_floor():
-		var player_position : float = (player.position.x - 3000) / 48
-		var player_position_fraction := player_position / seesaw_length
-		#rotation_speed += (MAX_ROTATION_SPEED * player_position_fraction) * delta
-		rotation_speed = lerp(rotation_speed, (MAX_ROTATION_SPEED**2) * player_position_fraction * delta, 1)
+	if ground_pound_timer > 0:
+		ground_pound_timer -= delta
 	else:
-		
-		rotation_speed = lerp(rotation_speed, (0 * delta), 0.01)
+		if player.is_on_floor():
+			var player_position : float = (player.position.x - 3000) / 48
+			var player_position_fraction := player_position / seesaw_length
+			rotation_speed = lerp(rotation_speed, (MAX_ROTATION_SPEED**2) * player_position_fraction * delta, 0.1)
+		else:
+			rotation_speed = lerp(rotation_speed, (0 * delta), 0.01)
 		
 	score_delta_tracker += delta * 10
 	if score_delta_tracker > 1:
 		score += floor(score_delta_tracker)
 		score_delta_tracker -= floor(score_delta_tracker)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ground_pound():
+	var player_position : float = (player.position.x - 3000) / 48
+	var player_position_fraction := player_position / seesaw_length
+	rotation_speed = (sign(player_position_fraction) * MAX_ROTATION_SPEED) + (player_position_fraction * MAX_ROTATION_SPEED * 8)
+	ground_pound_timer = 0.5
 
 func set_sewsaw_lengths():
 	seesaw.length = seesaw_length
