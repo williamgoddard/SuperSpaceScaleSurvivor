42fb3f21288c5255f69447dc39a6926813e4a901
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-08-20 00:36:04 +0100

    star spawning

--- scripts/Power_UP_Spawner.gd
+++ scripts/Power_UP_Spawner.gd
@@ -6,17 +6,19 @@ signal star_collected()
 @export var centre_node: Node2D
 @export var star_collector_node: Node2D
 
-func _ready():
-	$Timer.start()
-	pass
+var time_until_next_star := 30.0
 
+func _process(delta):
+	time_until_next_star -= delta
+	if time_until_next_star <= 0:
+		spawn_star()
+		time_until_next_star = randf_range(10, 20)
 
-func _on_timer_timeout():
+func spawn_star():
 	var power_up = power_up_scene.instantiate()
 	power_up.centre_node = centre_node
 	power_up.star_collector_node = star_collector_node
 	power_up.star_collected.connect(_on_star_collected)
-	print("timer timeout")
 	add_child(power_up)
 
 func _on_star_collected():
