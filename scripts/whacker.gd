665775e20b368d8590d087360e07a810abe4a537
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-08-19 22:25:04 +0100

    whacker animation

--- scripts/whacker.gd
+++ scripts/whacker.gd
@@ -1,14 +1,20 @@
 class_name Whacker extends StaticBody2D
 
+@onready var animated_sprite_2d := $AnimatedSprite2D
+
 const HIT_ENEMY_TIMEOUT = 2.0
 
 var hit_enemy := false
 var hit_enemy_timer := 0.0
 
 func destroy():
-	queue_free()
+	animated_sprite_2d.play("destroy")
+	collision_layer = 0
 
 func _process(delta):
-	
 	if hit_enemy:
 		hit_enemy_timer	+= delta
+
+func _on_animation_finished():
+	if animated_sprite_2d.animation == "destroy":
+		queue_free()
