b03a58fbd586a59e67c8ba209f35a23bb8d6bbe9
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2024-08-19 01:23:47 +0100

    recolour game over menu buttons

--- scripts/main_menu_button.gd
+++ scripts/main_menu_button.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
-const deselect_sprite = preload("res://assets/sprites/693xr583.bmp")
-const select_sprite = preload("res://assets/sprites/jh6zlhbm.bmp")
+#const deselect_sprite = preload("res://assets/sprites/693xr583.bmp")
+#const select_sprite = preload("res://assets/sprites/jh6zlhbm.bmp")
 
 @onready var label = $Label
 @onready var sprite = $Sprite2D
 
 @export var label_text := "OPTION"
+@export var text_colour := "FF5DD1"
 
 func _ready():
 	label.text = label_text
 
 func select():
-	sprite.texture = select_sprite
-	label.add_theme_color_override("font_color", "FF5DD1")
+	sprite.modulate = text_colour
+	label.add_theme_color_override("font_color", text_colour)
 	
 func deselect():
-	sprite.texture = deselect_sprite
+	sprite.modulate = "FFFFFF"
 	label.add_theme_color_override("font_color", "FFFFFF")
